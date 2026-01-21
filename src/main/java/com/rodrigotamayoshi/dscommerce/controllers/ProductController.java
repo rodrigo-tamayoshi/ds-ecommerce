5b450be0c217f1958b042e696e536cdd3ab189f0
rodrigo-tamayoshi <rodrigo.tamayoshi@gmail.com>
2026-01-21 12:33:39 -0300

    Adiciona PUT product

--- src/main/java/com/rodrigotamayoshi/dscommerce/controllers/ProductController.java
+++ src/main/java/com/rodrigotamayoshi/dscommerce/controllers/ProductController.java
@@ -47,4 +47,11 @@ public class ProductController {
 
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
 }
